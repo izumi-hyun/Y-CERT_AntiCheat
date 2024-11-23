e28c22e57cb4ae640b1cc4f1d28033d61ee9dfca
izumi-hyun <gyxo0302@gmail.com>
2024-11-23 19:36:19 +0900

    make_source

---
+++ Driver/MyDriver1/main.c
@@ -0,0 +1,201 @@
+﻿#include <ntifs.h>
+
+#define PROCESS_TERMINATE         0x0001  
+#define PROCESS_VM_OPERATION      0x0008  
+#define PROCESS_VM_READ           0x0010  
+#define PROCESS_VM_WRITE          0x0020  
+
+
+typedef NTSTATUS(NTAPI* pZwQueryInformationProcess)(
+    HANDLE ProcessHandle,
+    PROCESSINFOCLASS ProcessInformationClass,
+    PVOID ProcessInformation,
+    ULONG ProcessInformationLength,
+    PULONG ReturnLength
+    );
+
+pZwQueryInformationProcess ZwQueryInfoProc = NULL;
+
+typedef UCHAR* (NTAPI* PsGetProcessImageFileName_t)(PEPROCESS Process);
+PsGetProcessImageFileName_t PsGetProcessImageFileName = NULL;
+
+VOID DriverUnload(IN PDRIVER_OBJECT pDriverObj);
+NTSTATUS ProtectProcess();
+OB_PREOP_CALLBACK_STATUS preCall(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION pOperationInformation);
+UCHAR* GetProcessImageNameByProcessID(PEPROCESS  eProcess);
+
+PVOID obHandle = NULL; // 등록된 콜백 루틴을 식별하는 값을 받을 변수
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObj, IN PUNICODE_STRING pRegistryString)
+{
+    UNREFERENCED_PARAMETER(pRegistryString);
+    pDriverObj->DriverUnload = DriverUnload;
+    NTSTATUS status = ProtectProcess();
+    if (!NT_SUCCESS(status))
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "hello xxxx: 0x%x\n", status);
+    }
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS ProtectProcess()
+{
+    OB_CALLBACK_REGISTRATION obReg;
+    memset(&obReg, 0, sizeof(obReg));
+    obReg.Version = ObGetFilterVersion();
+    obReg.OperationRegistrationCount = 1;
+    obReg.RegistrationContext = NULL;
+
+    // Altitude 값 설정 (중복되지 않도록 고유한 값 사용)
+    RtlInitUnicodeString(&obReg.Altitude, L"321050");
+
+    OB_OPERATION_REGISTRATION opReg;
+    memset(&opReg, 0, sizeof(opReg));
+
+    // PsProcessType을 정확히 사용
+    opReg.ObjectType = PsProcessType;  // 변경: PsJobType -> PsProcessType
+    opReg.Operations = OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE;
+    opReg.PreOperation = (POB_PRE_OPERATION_CALLBACK)&preCall;
+
+    obReg.OperationRegistration = &opReg;
+
+    NTSTATUS status = ObRegisterCallbacks(&obReg, &obHandle);
+    if (!NT_SUCCESS(status))
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL,
+            "ObRegisterCallbacks failed, status: 0x%x\n", status);
+    }
+    else
+    {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL,
+            "ObRegisterCallbacks succeeded with Altitude: %wZ\n", &obReg.Altitude);
+    }
+
+    return status;
+}
+
+OB_PREOP_CALLBACK_STATUS preCall(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION pOperationInformation)
+{
+    UNREFERENCED_PARAMETER(RegistrationContext);
+    //DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "start_pre\n");
+
+    // ObjectType 확인
+    if (pOperationInformation->ObjectType != *PsProcessType) {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "sigong\n");
+        return OB_PREOP_SUCCESS; // 프로세스 타입이 아니면 처리하지 않음
+    }
+
+    //HANDLE pid = PsGetProcessId((PEPROCESS)pOperationInformation->Object);
+    //if (pid == NULL) {
+    //    DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "   \n");
+    //    DbgPrint("Failed to get process ID\n");
+    //    return OB_PREOP_SUCCESS;
+    //}
+
+    // 프로세스 이름 가져오기
+    PUCHAR szProcName = GetProcessImageNameByProcessID((PEPROCESS)pOperationInformation->Object);
+    if (szProcName == NULL) {
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "uzz\n");
+        //DbgPrint("Failed to get process name for PID: %lu\n", (ULONG)pid);
+        return OB_PREOP_SUCCESS;
+    }
+    //DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "uzz\n");
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, 0, "%s\n", szProcName);
+    DbgPrint("Intercepted process: Name: %s\n", szProcName);
+
+    // 특정 프로세스 이름 검사
+    if (!_stricmp((const char*)szProcName, "ac_client.exe")) {
+        if (pOperationInformation->Operation == OB_OPERATION_HANDLE_CREATE) {
+            ACCESS_MASK originalAccess = pOperationInformation->Parameters->CreateHandleInformation.OriginalDesiredAccess;
+
+            // 권한 제거
+            if (originalAccess & PROCESS_TERMINATE) {
+                pOperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~PROCESS_TERMINATE;
+            }
+            if (originalAccess & PROCESS_VM_OPERATION) {
+                pOperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~PROCESS_VM_OPERATION;
+            }
+            if (originalAccess & PROCESS_VM_READ) {
+                pOperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~PROCESS_VM_READ;
+            }
+            if (originalAccess & PROCESS_VM_WRITE) {
+                pOperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~PROCESS_VM_WRITE;
+            }
+        }
+    }
+
+    return OB_PREOP_SUCCESS;
+}
+
+
+UCHAR* GetProcessImageNameByProcessID(PEPROCESS  eProcess)
+{
+    //PEPROCESS EProcess = NULL;
+    UNICODE_STRING routineName;
+    RtlInitUnicodeString(&routineName, L"PsGetProcessImageFileName");
+    PsGetProcessImageFileName = (PsGetProcessImageFileName_t)MmGetSystemRoutineAddress(&routineName);
+    if (PsGetProcessImageFileName == NULL) {
+        DbgPrint("Failed to get PsGetProcessImageFileName address\n");
+        ObDereferenceObject(eProcess);
+        return NULL;
+    }
+    return PsGetProcessImageFileName(eProcess);
+
+    //Status = ObOpenObjectByPointer(EProcess, 0, NULL, PROCESS_ALL_ACCESS, PsProcessType, KernelMode, &processHandle);
+    //if (!NT_SUCCESS(Status)) {
+    //    DbgPrint("Failed to open process object by pointer: %lu, Status: 0x%x\n", ulProcessID, Status);
+    //    ObDereferenceObject(EProcess);
+    //    return NULL;
+    //}
+
+    //// ZwQueryInformationProcess 사용
+    //PROCESS_BASIC_INFORMATION pbi;
+    //ULONG returnLength = 0;
+
+    //if (ZwQueryInfoProc == NULL) {
+    //    UNICODE_STRING routineName;
+    //    RtlInitUnicodeString(&routineName, L"ZwQueryInformationProcess");
+    //    ZwQueryInfoProc = (pZwQueryInformationProcess)MmGetSystemRoutineAddress(&routineName);
+    //    if (ZwQueryInfoProc == NULL) {
+    //        DbgPrint("Failed to get ZwQueryInformationProcess address\n");
+    //        ObDereferenceObject(EProcess);
+    //        return NULL;
+    //    }
+    //}
+    //PVOID get_buffer = NULL;
+    //ULONG process_FULL__NAME_info_len = 0;
+    //
+    //while (ZwQueryInfoProc(processHandle, ProcessImageFileName, get_buffer, process_FULL__NAME_info_len, &process_FULL__NAME_info_len) == STATUS_INFO_LENGTH_MISMATCH) {
+    //    if (get_buffer != NULL) {
+    //        break;
+    //    }
+    //    get_buffer = ExAllocatePoolWithTag(NonPagedPool, process_FULL__NAME_info_len, 'PrNm');
+    //}
+
+    //if (get_buffer == NULL) {
+    //    return STATUS_UNSUCCESSFUL;
+    //}
+
+    //PUNICODE_STRING process_image_file_name = (PUNICODE_STRING)get_buffer;
+    //if (process_image_file_name->Buffer == NULL) {
+    //    ExFreePoolWithTag(get_buffer, 'PrNm');
+    //    return STATUS_UNSUCCESSFUL;
+    //}
+
+    //ObDereferenceObject(EProcess);
+    //return processName;
+}
+
+VOID DriverUnload(IN PDRIVER_OBJECT pDriverObj)
+{
+    UNREFERENCED_PARAMETER(pDriverObj);
+    DbgPrint("Driver unloading...\n");
+
+    if (obHandle)
+    {
+        ObUnRegisterCallbacks(obHandle);
+        obHandle = NULL;
+    }
+
+    DbgPrint("Driver unloaded successfully.\n");
+}
---
+++ inject_dll/main.cpp
@@ -0,0 +1,206 @@
+#include <windows.h>
+#include <iostream>
+#include <tlhelp32.h>
+#include <string>
+
+using namespace std;
+
+// 콘솔 생성 함수
+//void CreateConsole() {
+//    AllocConsole();
+//
+//    FILE* fp;
+//    freopen_s(&fp, "CONOUT$", "w", stdout);
+//    freopen_s(&fp, "CONIN$", "r", stdin);
+//    freopen_s(&fp, "CONOUT$", "w", stderr);
+//
+//    std::cout << "콘솔 창이 생성되었습니다." << std::endl;
+//}
+
+extern "C" __declspec(dllexport) bool CheckWindowByName(LPCSTR NameWindow) {
+    HWND hwnd = FindWindowA(NULL, NameWindow);
+    if (hwnd != NULL) {
+        MessageBoxA(NULL, "디버거 감지!", "디버거 감지", MB_OK); // MessageBoxA 사용
+        return true;
+    }
+    return false;
+}
+
+
+// 프로세스 이름 확인
+void CheckProcessByName(const wchar_t* ProcName) {
+    PROCESSENTRY32W pe32;
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnapshot == INVALID_HANDLE_VALUE)
+        return;
+
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+    if (Process32FirstW(hSnapshot, &pe32)) {
+        do {
+            if (wcscmp(pe32.szExeFile, ProcName) == 0) {
+                MessageBoxW(NULL, L"프로세스 감지!!", L"프로세스 감지", MB_OK);
+                break;
+            }
+        } while (Process32NextW(hSnapshot, &pe32));
+    }
+    CloseHandle(hSnapshot);
+}
+
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+
+void CheckDebuggerUsingNtQuery() {
+    typedef NTSTATUS(WINAPI* NtQueryInformationProcess)(HANDLE, ULONG, PVOID, ULONG, PULONG);
+
+    DWORD debugPort = 0; // 32비트 시스템에서는 DWORD 사용
+    HMODULE hNtdll = GetModuleHandle(L"ntdll.dll");
+    if (hNtdll) {
+        NtQueryInformationProcess NtQueryInfo = (NtQueryInformationProcess)GetProcAddress(hNtdll, "NtQueryInformationProcess");
+        if (NtQueryInfo) {
+            NTSTATUS status = NtQueryInfo(GetCurrentProcess(), 7, &debugPort, sizeof(debugPort), NULL);
+            if (NT_SUCCESS(status) && debugPort) {
+                MessageBox(NULL, L"NtQuery 디버거 감지!! [NtQIP]", L"디버거 감지", MB_OK);
+            }
+        }
+    }
+}
+
+void DetectSoftwareBreakpoint() {
+    int flag = 0;
+    __try {
+        __asm { int 3 }  // 소프트웨어 브레이크포인트 명령어
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        flag = 1; // 예외 발생 시 플래그 설정
+    }
+
+    if (flag == 0) {
+        MessageBox(NULL, L"SBP 디버거 감지 [int 3]", L"디버거 감지", MB_OK);
+    }
+}
+
+extern "C" __declspec(dllexport) void MemoryScan(int addr, int length) {
+    BYTE* currentBuffer = new BYTE[length];  // 메모리 검사 버퍼
+    BYTE* originalBuffer = new BYTE[length]; // 원래 값의 버퍼
+
+    // 원본 값을 메모리에서 직접 읽어오기
+    if (!ReadProcessMemory(GetCurrentProcess(), (LPCVOID)addr, originalBuffer, length, NULL)) {
+        MessageBox(NULL, L"메모리 읽기 실패", L"오류", MB_OK);
+        delete[] currentBuffer;
+        delete[] originalBuffer;
+        return;
+    }
+
+    while (true) {
+        // 100ms 대기
+        Sleep(100);
+
+        // 메모리 읽기
+        if (!ReadProcessMemory(GetCurrentProcess(), (LPCVOID)addr, currentBuffer, length, NULL)) {
+            MessageBox(NULL, L"메모리 읽기 실패", L"오류", MB_OK);
+            break;
+        }
+
+        // 메모리 변조 검사
+        bool isModified = false;
+        for (int i = 0; i < length; i++) {
+            if (currentBuffer[i] != originalBuffer[i]) {
+                isModified = true;
+                break;
+            }
+        }
+
+        // 메모리가 변조되었다면
+        if (isModified) {
+            MessageBox(NULL, L"메모리 변조 감지", L"메모리", MB_OK);
+            //break; // 변조되고 원래는 무한루프가 종료됨
+        }
+    }
+
+    // 메모리 해제
+    delete[] currentBuffer;
+    delete[] originalBuffer;
+}
+struct MemoryScanParams {
+    int addr;       // 메모리 검사 시작 주소
+    int length;     // 검사할 바이트 수
+};
+
+DWORD WINAPI MemoryScanThread(LPVOID lpParam) {
+    int* data = (int*)lpParam;
+    int addr = data[0];        // 시작 주소
+    int length = data[1];      // 검사할 바이트 수
+
+    // 메모리 검사 함수 호출
+    MemoryScan(addr, length);
+
+    // 메모리 해제
+    delete[] data; // 메모리 해제
+
+    return 0;
+}
+
+// 메모리 검사 스레드 시작 함수
+extern "C" __declspec(dllexport) void StartMemoryScan() {
+    // 메모리 검사 작업을 스레드로 실행
+    int* data = new int[2];  // 배열로 데이터를 전달
+    int* data2 = new int[2];  // 배열로 데이터를 전달
+    int* data3 = new int[2];  // 배열로 데이터를 전달
+
+    // 메모리 검사 시작 주소와 길이 설정
+    data[0] = (int)GetModuleHandleA("ac_client.exe") + 0xC73EA;  // 시작 주소 난사
+    data2[0] = (int)GetModuleHandleA("ac_client.exe") + 0xC2EC3;  // 시작 주소 난사
+    
+    data3[0] = (int)GetModuleHandleA("ac_client.exe") + 0xC73EF;  // 시작 주소 총알치트
+    
+    data[1] = 2;  // 검사할 바이트 수
+    data2[1] = 5;  // 검사할 바이트 수
+    data3[1] = 2; // 검사할 바이트 수
+    // 스레드 시작
+    CreateThread(NULL, 0, MemoryScanThread, (LPVOID)data, 0, NULL);
+    CreateThread(NULL, 0, MemoryScanThread, (LPVOID)data2, 0, NULL);
+    CreateThread(NULL, 0, MemoryScanThread, (LPVOID)data3, 0, NULL);
+}
+// 무한 루프를 스레드로 처리
+
+
+DWORD WINAPI DetectDebuggerThread(LPVOID lpParam) {
+    // 무한 루프
+    while (true) {
+        // IsDebuggerPresent 호출
+        if (IsDebuggerPresent()) {
+            MessageBox(NULL, L"IsDebuggerPresent 디버거 감지!!", L"디버거 감지", MB_OK);
+        }
+
+        //프로세스 이름 감지
+        CheckProcessByName(L"cheatengine-x86_64-SSE4-AVX2.exe"); // 예시: 치트엔진 프로세스 감지
+
+        // NtQuery 정보 확인
+        CheckDebuggerUsingNtQuery();
+
+        // 소프트웨어 브레이크포인트 감지
+        DetectSoftwareBreakpoint();
+
+        // 잠시 대기
+        Sleep(1000);  // 1000ms 대기
+    }
+    return 0;
+}
+// DLL 메인 함수
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        // 콘솔 생성
+        //CreateConsole();
+
+        // 메모리변조 감지 함수
+        StartMemoryScan();
+        // 디버깅 탐지 스레드 생성
+        CreateThread(NULL, 0, DetectDebuggerThread, NULL, 0, NULL);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        //FreeConsole(); // 콘솔 해제
+        break;
+    }
+    return TRUE;
+}
